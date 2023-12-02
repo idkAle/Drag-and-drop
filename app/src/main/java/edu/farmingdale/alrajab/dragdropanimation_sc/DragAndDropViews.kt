c7efebf0fb1aaad70b81dd98520dd9180adb7621
Alejandro Cerrato <cerra@farmingdale.edu>
2023-12-01 19:55:50 -0500

    Drag and Drop

--- app/src/main/java/edu/farmingdale/alrajab/dragdropanimation_sc/DragAndDropViews.kt
+++ app/src/main/java/edu/farmingdale/alrajab/dragdropanimation_sc/DragAndDropViews.kt
@@ -4,6 +4,7 @@ import android.content.ClipData
 import android.content.ClipDescription
 import android.graphics.Canvas
 import android.graphics.Point
+import android.graphics.drawable.AnimationDrawable
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -16,18 +17,38 @@ import edu.farmingdale.alrajab.dragdropanimation_sc.databinding.ActivityDragAndD
 
 class DragAndDropViews : AppCompatActivity() {
     lateinit var binding: ActivityDragAndDropViewsBinding
+    lateinit var birdImageView: ImageView
+    lateinit var animationDrawable: AnimationDrawable
+    lateinit var startStopButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDragAndDropViewsBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
+
+        birdImageView = findViewById(R.id.bird)
+        animationDrawable = birdImageView.background as AnimationDrawable
+
+        // Initialize the Start/Stop button after setContentView
+        startStopButton = findViewById(R.id.startStopButton)
+
+        // Set a click listener for the Start/Stop button
+        startStopButton.setOnClickListener {
+            toggleAnimation()
+        }
+
+        // Drag Listener for all holders
         binding.holder01.setOnDragListener(arrowDragListener)
         binding.holder02.setOnDragListener(arrowDragListener)
+        binding.holder03.setOnDragListener(arrowDragListener)
+        binding.holder04.setOnDragListener(arrowDragListener)
+        binding.holder05.setOnDragListener(arrowDragListener)
 
-
+        // Listeners for all arrows
         binding.upMoveBtn.setOnLongClickListener(onLongClickListener)
-
-
+        binding.downMoveBtn.setOnLongClickListener(onLongClickListener)
+        binding.forwardMoveBtn.setOnLongClickListener(onLongClickListener)
+        binding.backMoveBtn.setOnLongClickListener(onLongClickListener)
 
     }
 
@@ -63,9 +84,13 @@ class DragAndDropViews : AppCompatActivity() {
                     return@OnDragListener true
                 }
                 DragEvent.ACTION_DRAG_ENTERED -> {
+                    // Highlight the holder when hovering over it
+                    view.setBackgroundResource(R.drawable.higlight_border)
                     return@OnDragListener true
                 }
                 DragEvent.ACTION_DRAG_EXITED-> {
+                    // Unhighlight if the user is no hovering over holder
+                    view.setBackgroundResource(R.drawable.border)
                     return@OnDragListener true
                 }
                 // No need to handle this for our use case.
@@ -77,9 +102,17 @@ class DragAndDropViews : AppCompatActivity() {
                     // Read color data from the clip data and apply it to the card view background.
                     val item: ClipData.Item = dragEvent.clipData.getItemAt(0)
                     val lbl = item.text.toString()
+                    view.setBackgroundResource(R.drawable.border)
+
+
                     Log.d("BCCCCCCCCCCC", "NOTHING > >  " + lbl)
                    when(lbl.toString()){
                        "UP"->view.setImageResource( R.drawable.ic_baseline_arrow_upward_24)
+                       "DOWN"->view.setImageResource( R.drawable.ic_baseline_arrow_downward_24)
+                       "BACK"->view.setImageResource( R.drawable.ic_baseline_arrow_back_24)
+                       "FORWARD"->view.setImageResource( R.drawable.ic_baseline_arrow_forward_24)
+
+
                    }
                     return@OnDragListener true
                 }
@@ -104,6 +137,19 @@ class DragAndDropViews : AppCompatActivity() {
         }
         override fun onDrawShadow(canvas: Canvas) {
             shadow?.draw(canvas)
+        }
+    }
+
+
+    private fun toggleAnimation() {
+        if (animationDrawable.isRunning) {
+            // If the animation is running, stop it and update the button text
+            animationDrawable.stop()
+            startStopButton.text = "Start"
+        } else {
+            // If the animation is not running, start it and update the button text
+            animationDrawable.start()
+            startStopButton.text = "Stop"
         }
     }
 }
